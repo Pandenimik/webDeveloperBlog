2a13d9e2d0cd2e6242dfd24256e913770833fb5e
Pandenimik <pandenimik@mail.ru>
2022-04-19 13:37:15 +0300

    Initial commit

---
+++ game/script.js
@@ -0,0 +1,19 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener("keydown", function(event) {
+  jump();
+});
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+  }
+  setTimeout(function() {
+    dino.classList.remove("jump")
+  }, 800);
+}
+
+let isAlive = setInterval ( function() {
+  
+});
